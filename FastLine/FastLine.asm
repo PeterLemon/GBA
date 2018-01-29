656a171876d3f3076ef93c7b68c85eed2286c652
PeterLemon <krom@gbadev.org>
2018-01-29 10:44:29 +0000

    Optimized Fast Line

--- FastLine/FastLine.asm
+++ FastLine/FastLine.asm
@@ -1,4 +1,5 @@
 ; Game Boy Advance 'Bare Metal' Fast Line Plot Demo by krom (Peter Lemon):
+; Uses L/R/A/B To Change Line Start Point X/Y Position
 ; Uses Direction Pad To Change Line End Point X/Y Position
 
 format binary as 'gba'
@@ -12,74 +13,90 @@ b copycode
 times $80000C0-($-0) db 0
 
 macro Control {
-  ldrb r0,[LineX]
-  IsKeyDown KEY_RIGHT
-  addeq r0,1
-  strbeq r0,[LineX]
+  ldrb r0,[LineX1] ; R0 = Line Point X1
+  IsKeyDown KEY_R ; IF (R Key Pressed)
+  addeq r0,1 ; Line Point X1++
+  strbeq r0,[LineX1] ; Line Point X1 = R0
 
-  IsKeyDown KEY_LEFT
-  subeq r0,1
-  strbeq r0,[LineX]
+  IsKeyDown KEY_L ; IF (L Key Pressed)
+  subeq r0,1 ; Line Point X1--
+  strbeq r0,[LineX1] ; Line Point X1 = R0
 
-  ldrb r0,[LineY]
-  IsKeyDown KEY_DOWN
-  addeq r0,1
-  strbeq r0,[LineY]
+  ldrb r0,[LineY1] ; R0 = Line Point Y1
+  IsKeyDown KEY_B ; IF (B Key Pressed)
+  addeq r0,1 ; Line Point Y1++
+  strbeq r0,[LineY1] ; Line Point Y1 = R0
 
-  IsKeyDown KEY_UP
-  subeq r0,1
-  strbeq r0,[LineY]
+  IsKeyDown KEY_A ; IF (A Key Pressed)
+  subeq r0,1 ; Line Point Y1--
+  strbeq r0,[LineY1] ; Line Point Y1 = R0
+
+  ldrb r0,[LineX2] ; R0 = Line Point X2
+  IsKeyDown KEY_RIGHT ; IF (Right Key Pressed)
+  addeq r0,1 ; Line Point X2++
+  strbeq r0,[LineX2] ; Line Point X2 = R0
+
+  IsKeyDown KEY_LEFT ; IF (Left Key Pressed)
+  subeq r0,1 ; Line Point X2--
+  strbeq r0,[LineX2] ; Line Point X2 = R0
+
+  ldrb r0,[LineY2] ; R0 = Line Point Y2
+  IsKeyDown KEY_DOWN ; IF (Down Key Pressed)
+  addeq r0,1 ; Line Point Y2++
+  strbeq r0,[LineY2] ; Line Point Y2 = R0
+
+  IsKeyDown KEY_UP ; IF (Up Key Pressed)
+  subeq r0,1 ; Line Point Y2--
+  strbeq r0,[LineY2] ; Line Point Y2 = R0
 }
 
-macro DrawLine X0, Y0, X1, Y1, Colour {
+macro DrawLine x1, y1, x2, y2, color { ; Draw Line: Line Point X1, Line Point Y1, Line Point X2, Line Point Y2, Line Color
   local .LoopX, .LoopY, .LineEnd
-  mov r0,X0 ; Moves X0 To R0 (X0)
-  mov r1,Y0 ; Moves Y0 To R1 (Y0)
-  mov r2,X1 ; Moves X1 To R2 (X1)
-  mov r3,Y1 ; Moves Y1 To R3 (Y1)
-  mov r8,240 ; Moves 240 To R8 (Screen Width)
-  ldr r9,[Colour] ; Load Color To R9 (Color)
-  mov r10,VRAM ; Load VRAM Base Pointer To R9 (VRAM Base Pointer)
-  
-  subs r4,r2,r0 ; Subtract R0 (X0) From R4 (DX) & Compare R4 (DX) To Zero (X1 - X0)
-  rsbmi r4,0 ; Convert R4 (DX) To ABS(DX)
-  mvnlt r6,0 ; IF (X1 < X0), R6 (SX) = -1
-  movgt r6,1 ; IF (X1 > X0), R6 (SX) =	1
 
-  subs r5,r3,r1 ; Subtract R2 (Y0) From R5 (DY) & Compare R5 (DY) To Zero (Y1 - Y0)
-  rsbmi r5,0 ; Convert R5 (DY) To ABS(DX)
-  mvnlt r7,0 ; IF (Y1 < Y0), R7 (SY) = -1
-  movgt r7,1 ; IF (Y1 > Y0), R7 (SY) =	1
+  subs r0,x2,x1 ; R0 = DX (X2 - X1), Compare DX With Zero
+  rsbmi r0,0 ; R0 = ABS(DX)
+  mvnlt r2,1 ; IF (X2 < X1), R2 = -2 (SX)
+  movgt r2,2 ; IF (X2 > X1), R2 = 2 (SX)
 
-  cmp r4,r5 ; Compare DX To DY
-  movgt r3,r4,lsr 1 ; IF (DX >	DY), R3 (X Error) = R4 (DX) / 2 (X Error = DX / 2)
-  movle r2,r5,lsr 1 ; IF (DX <= DY), R1 (Y Error) = R5 (DY) / 2 (Y Error = DY / 2)
+  subs r1,y2,y1 ; R1 = DY (Y2 - Y1), Compare DY With Zero
+  rsbmi r1,0 ; R1 = ABS(DX)
+  movlt r3,0 ; IF (Y2 < Y1) R3 = -480 (SY)
+  sublt r3,480
+  movgt r3,480 ; IF (Y2 > Y1) R3 = 480 (SY)
+
+  mov r4,240 ; R4 = 240 (Screen Width)
+
+  mla x1,y1,r4,x1 ; X1 = (Y1 * Screen Width) + X1
+  lsl x1,1 ; X1 <<= 1
+  add x1,VRAM ; X1 = Point Start
+
+  mla y1,y2,r4,x2 ; Y1 = (Y2 * Screen Width) + X2
+  lsl y1,1 ; Y1 <<= 1
+  add y1,VRAM ; Y1 = Point End
+
+  cmp r0,r1 ; Compare DX To DY
+  lsrgt x2,r0,1 ; IF (DX >  DY) X2 = DX / 2 (X Error)
+  lsrle y2,r1,1 ; IF (DX <= DY) Y2 = DY / 2 (Y Error)
   ble .LoopY
 
   .LoopX:
-    mla r11,r1,r8,r0 ; Multiplies R2 (Y0) By R8 (Screen Width) To R2 (Y Byte Position), Add R8 To R0 (X0) To R11 (XY Byte Position)
-    lsl r11,1 ; Double R11 (XY Byte Position) To R11 (XY Half Word Position)
-    strh r9,[r10,r11] ; Store R9 (Colour) To R10 (VRAM Base Pointer) Added To R11 (XY VRAM Position)
-
-    cmp r0,r2 ; While (X0 != X1)
-    beq .LineEnd ; IF (X0 == X1), Branch To Line End
-    subs r3,r5	; Subtract R5 (DY) From R3 (X Error) & Compare R3 (X Error) To Zero (X Error -= DY)
-    addlt r1,r7 ; IF (X Error < 0), Add R7 (SY) To R1 (Y0) (Y0 += SY)
-    addlt r3,r4 ; IF (X Error < 0), Add R4 (DX) To R3 (X Error) (X Error += DX)
-    add r0,r6 ; Add R6 (SX) To R0 (X0) (X0 += SX)
+    strh color,[x1] ; Store Color To Point Start
+    cmp x1,y1 ; Compare Point Start To Point End
+    beq .LineEnd ; IF (Point Start == Point End) Line End
+    subs x2,r1  ; X Error -= DY, Compare X Error To Zero
+    addlt x1,r3 ; IF (X Error < 0) Point Start += SY
+    addlt x2,r0 ; IF (X Error < 0) X Error += DX
+    add x1,r2 ; Point Start += SX
     b .LoopX ; Loop Line Drawing
 
   .LoopY:
-    mla r11,r1,r8,r0 ; Multiplies R2 (Y0) By R8 (Screen Width) To R2 (Y Byte Position), Add R8 To R0 (X0) To R11 (XY Byte Position)
-    lsl r11,1 ; Double R11 (XY Byte Position) To R11 (XY Half Word Position)
-    strh r9,[r10,r11] ; Store R9 (Colour) To R10 (VRAM Base Pointer) Added To R11 (XY VRAM Position)
-
-    cmp r1,r3 ; While (Y0 != Y1)
-    beq .LineEnd ; IF (Y0 == Y1), Branch To Line End
-    subs r2,r4	; Subtract R4 (DX) From R1 (Y Error) & Compare R1 (Y Error) To Zero
-    addlt r0,r6 ; IF (Y Error < 0), Add R6 (SX) To R0 (X0) (X0 += SX)
-    addlt r2,r5 ; IF (Y Error < 0), Add R5 (DY) To R2 (Y Error) (Y Error += DY)
-    add r1,r7 ; Add R7 (SY) To R1 (Y0) (Y0 += SY)
+    strh color,[x1] ; Store Color To Point Start
+    cmp x1,y1 ; Compare Point Start To Point End
+    beq .LineEnd ; IF (Point Start == Point End) Line End
+    subs y2,r0  ; Y Error -= DX, Compare Y Error To Zero
+    addlt x1,r2 ; IF (Y Error < 0) Point Start += SX
+    addlt y2,r1 ; IF (Y Error < 0) Y Error += DY
+    add x1,r3 ; Point Start += SY
     b .LoopY ; Loop Line Drawing
 
   .LineEnd: ; End of Line Drawing
@@ -100,10 +117,12 @@ startcode:
   org IWRAM
 
 ; Variable Data
-Black: dw $00000000 ; Black Clear Color
-White: dw $0000FFFF ; White Line Color
-LineX: db 239 ; Line X Amount: 239 Pixels Across
-LineY: db 159 ; Line Y Amount: 159 Pixels Down
+Black: dw $00000000 ; Clear Color (Black)
+White: dw $0000FFFF ; Line Color (White)
+LineX1: db 120 ; Line X1: 120 Pixels Across
+LineY1: db 80  ; Line Y1:  80 Pixels Down
+LineX2: db 239 ; Line X2: 239 Pixels Across
+LineY2: db 159 ; Line Y2: 159 Pixels Down
 
   align 4
 start:
@@ -112,14 +131,18 @@ start:
   orr r1,BG2_ENABLE
   str r1,[r0]
 
-loop:
-  VBlank
-  Control
-  DMA32SRCFIXED Black, VRAM, 19200 ; Clear Screen
+ldr r10,[White] ; R10 = Line Color (White)
 
-  ldrb r10,[LineX]
-  ldrb r11,[LineY]
-  DrawLine 120, 80, r10, r11, White
+loop:
+  VBlank ; Wait For VBlank
+  Control ; Run Control
+  DMA32SRCFIXED Black, VRAM, 19200 ; Clear Screen Using DMA
+
+  ldrb r6,[LineX1]
+  ldrb r7,[LineY1]
+  ldrb r8,[LineX2]
+  ldrb r9,[LineY2]
+  DrawLine r6, r7, r8, r9, r10 ; Draw Line: Line Point X1, Line Point Y1, Line Point X2, Line Point Y2, Line Color
   b loop
 
 endcopy: ; End Of Program Copy Code

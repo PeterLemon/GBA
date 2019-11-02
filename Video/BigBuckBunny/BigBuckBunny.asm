fa092f94e8ed4ea20c1e26955f3a877ab00b4c7c
peterlemon <krom@gbadev.org>
2019-11-02 07:36:52 +0000

    LZSS DIFF RLE GRB 12-bit Compression

--- Video/BigBuckBunny/BigBuckBunny.asm
+++ Video/BigBuckBunny/BigBuckBunny.asm
@@ -1,4 +1,4 @@
-; GBA 'Bare Metal' GRB 12-Bit LZ Compressed IPS Video Decode 120x80 Demo by krom (Peter Lemon):
+; GBA 'Bare Metal' GRB 12-Bit LZSS DIFF RLE Video Decode 120x80 Demo by krom (Peter Lemon):
 
 format binary as 'gba'
 include 'LIB\FASMARM.INC'
@@ -11,39 +11,47 @@ org $8000000
 b copycode
 times $80000C0-($-0) db 0
 
-macro IPSDecode { ; Decode IPS Headerless & Footerless Data With 2 Byte (Word) Offsets, Little Endian Offset, Size, & RLE Size
-  local .IPSLoop, .IPSData, .IPSRLE, .IPSRLELoop, .IPSEOF
-  imm32 r0,WRAM + 6300 ; R0 = IPS Location
-  mov r5,WRAM ; R5 = WRAM Offset
-  ldr r6,[IPSOffset] ; R6 = IPS WRAM End Offset
-  .IPSLoop: ; Decode IPS Offset
-    ldrb r2,[r0],1 ; R2 = IPS Offset Word Lo Byte
-    ldrb r4,[r0],1 ; R4 = IPS Offset Word Hi Byte
-    orr r2,r4,lsl 8 ; R2 = IPS Offset Word
-    add r1,r5,r2 ; R1 = WRAM Offset + IPS Offset
-  ; Decode IPS Size
-    ldrb r3,[r0],1 ; R3 = IPS Size Word Lo Byte
-    ldrb r4,[r0],1 ; R4 = IPS Size Word Hi Byte
-    orrs r3,r4,lsl 8 ; R3 = IPS Size Word
-    beq .IPSRLE ; If (IPS Size == 0) Decode IPS RLE
-  .IPSData: ; Decode IPS Data
-    ldrb r4,[r0],1 ; R4 = Data Byte
-    strb r4,[r1],1 ; Store Data Byte To WRAM
-    subs r3,1 ; IPS Size--
-    bne .IPSData ; IF (IPS Size != 0) Loop IPS Data
-    beq .IPSEOF  ; ELSE GOTO IPS EOF
-  .IPSRLE: ; Decode IPS RLE Data
-    ldrb r3,[r0],1 ; R3 = IPS RLE Size Word Lo Byte
-    ldrb r4,[r0],1 ; R4 = IPS RLE Size Word Hi Byte
-    orr r3,r4,lsl 8 ; R3 = IPS RLE Size Word
-    ldrb r4,[r0],1 ; R4 = RLE Repeat Byte
-    .IPSRLELoop: ; IPS RLE Repeat Byte Data Loop
-      strb r4,[r1],1 ; Store RLE Repeat Byte To WRAM
-      subs r3,1 ; IPS RLE Size--
-      bne .IPSRLELoop ; IF (IPS RLE Size != 0) Loop IPS RLE Byte Copy
-  .IPSEOF: ; Decode IPS EOF
-    cmp r0,r6 ; Compare WRAM Offset To IPS WRAM End Offset
-    bne .IPSLoop ; IF (WRAM Offset != IPS WRAM End Offset) Loop IPS Decoding
+macro RLEDecode { ; Decode RLE DIFF Data
+  local .RLELoop, .RLECopy, .RLEDecode, .RLEDecodeByte, .RLEEOF
+  imm32 r0,WRAM + 6300 + 4 ; R0 = RLE Source Offset + 4
+  mov r1,WRAM ; R1 = Destination Address (WRAM Start Offset)
+  imm32 r2,WRAM + 6300 ; R2 = Destination End Offset (WRAM End Offset)
+
+  .RLELoop:
+    cmp r1,r2 ; Compare Destination Address To Destination End Offset
+    beq .RLEEOF ; IF (Destination Address == Destination End Offset) RLEEOF
+
+    ldrb r3,[r0],1 ; R3 = RLE Flag Data (Bit 0..6 = Expanded Data Length: Uncompressed N-1, Compressed N-3, Bit 7 = Flag: 0 = Uncompressed, 1 = Compressed)
+    ands r4,r3,10000000b ; R4 = RLE Flag
+    and r3,01111111b ; R3 = Expanded Data Length
+    add r3,1 ; Expanded Data Length++
+    bne .RLEDecode ; IF (BlockType != 0) RLE Decode Bytes
+
+    .RLECopy: ; ELSE Copy Uncompressed Bytes
+      ldrsb r4,[r0],1 ; R4 = Difference Signed Byte, RLE Offset++
+      ldrsb r5,[r1] ; R5 = Source Data Byte
+      add r5,r4 ; Add Signed Byte (R4) To Data Byte (R5)
+      strb r5,[r1],1 ; Store Uncompressed Byte To Destination, Destination Address++
+      subs r3,1 ; Expanded Data Length--
+      bne .RLECopy ; IF (Expanded Data Length != 0) RLECopy
+      b .RLELoop
+
+    .RLEDecode:
+      add r3,2 ; Expanded Data Length += 2
+      ldrsb r4,[r0],1 ; R4 = Difference Signed Byte, RLE Offset++
+      cmp r4,0 ; Compare Difference Signed Byte To 0
+      bne .RLEDecodeByte ; IF (Difference Signed Byte != 0) RLEDecodeByte
+      add r1,r3 ; ELSE Skip RLEDecodeByte, Add Expanded Data Length (R3) To WRAM Offset
+      b .RLELoop
+
+      .RLEDecodeByte:
+        ldrsb r5,[r1] ; R5 = Source Data Byte
+        add r5,r4 ; Add Signed Byte (R4) To Data Byte (R5)
+        strb r5,[r1],1 ; Store Uncompressed Byte To Destination, Destination Address++
+        subs r3,1 ; Expanded Data Length--
+        bne .RLEDecodeByte ; IF (Expanded Data Length != 0) RLEDecodeByte
+        b .RLELoop
+    .RLEEOF:
 }
 
 macro GRBDecode { ; Decode GRB Frame
@@ -383,8 +391,14 @@ start:
 
   PlaySoundA SOUND, 22050 ; Play Sound Channel A Data
 LoopFrames:
+  ; Start Timer
+  mov r11,IO ; GBA I/O Base Offset
+  orr r11,TM1CNT ; Timer Control Register
+  mov r12,TM_ENABLE or TM_FREQ_1024 ; Timer Enable, Frequency/1024
+  str r12,[r11] ; Start Timer
+
   ldr r0,[LZOffset] ; R0 = LZ Offset
-  imm32 r1,WRAM + 6300 ; R1 = LZ IPS File Output Offset
+  imm32 r1,WRAM + 6300 ; R1 = Destination Offset
   ldr r2,[r0],4 ; R2 = Data Length & Header Info
   lsr r2,8 ; R2 = Data Length
   add r2,r1 ; R2 = Destination End Offset
@@ -404,46 +418,37 @@ LoopFrames:
       b LZBlockLoop
 
       LZDecode:
-	ldrb r5,[r0],1 ; R5 = Number Of Bytes To Copy & Disp MSB's
-	ldrb r6,[r0],1 ; R6 = Disp LSB's
-	add r6,r5,lsl 8
-	lsr r5,4 ; R5 = Number Of Bytes To Copy (Minus 3)
-	add r5,3 ; R5 = Number Of Bytes To Copy
-	mov r7,$1000
-	sub r7,1 ; R7 = $FFF
-	and r6,r7 ; R6 = Disp
-	add r6,1 ; R6 = Disp + 1
-	rsb r6,r1 ; R6 = Destination - Disp - 1
-	LZCopy:
-	  ldrb r7,[r6],1 ; R7 = Byte To Copy
-	  strb r7,[r1],1 ; Store Byte To WRAM
-	  subs r5,1 ; Number Of Bytes To Copy -= 1
-	  bne LZCopy ; IF (Number Of Bytes To Copy != 0) LZ Copy Bytes
-	  b LZBlockLoop
+        ldrb r5,[r0],1 ; R5 = Number Of Bytes To Copy & Disp MSB's
+        ldrb r6,[r0],1 ; R6 = Disp LSB's
+        add r6,r5,lsl 8
+        lsr r5,4 ; R5 = Number Of Bytes To Copy (Minus 3)
+        add r5,3 ; R5 = Number Of Bytes To Copy
+        mov r7,$1000
+        sub r7,1 ; R7 = $FFF
+        and r6,r7 ; R6 = Disp
+        add r6,1 ; R6 = Disp + 1
+        rsb r6,r1 ; R6 = Destination - Disp - 1
+        LZCopy:
+          ldrb r7,[r6],1 ; R7 = Byte To Copy
+          strb r7,[r1],1 ; Store Byte To WRAM
+          subs r5,1 ; Number Of Bytes To Copy -= 1
+          bne LZCopy ; IF (Number Of Bytes To Copy != 0) LZ Copy Bytes
+          b LZBlockLoop
     LZEnd:
-
-  str r1,[IPSOffset] ; Store IPS WRAM End Offset
 
   ; Skip Zero's At End Of LZ Compressed File
   ands r1,r0,3 ; Compare LZ Offset To A Multiple Of 4
   subne r0,r1 ; IF (LZ Offset != Multiple Of 4) Add R1 To the LZ Offset
   addne r0,4 ; LZ Offset += 4
 
-  str r0,[LZOffset] ; Store Last LZ IPS Frame End Offset To LZ Offset
+  str r0,[LZOffset] ; Store Last LZ Frame End Offset To LZ Offset
 
-  ; Start Timer
-  mov r11,IO ; GBA I/O Base Offset
-  orr r11,TM1CNT ; Timer Control Register
-  mov r12,TM_ENABLE or TM_FREQ_1024 ; Timer Enable, Frequency/1024
-  str r12,[r11] ; Start Timer
-
-  IPSDecode ; Decode IPS Data To WRAM
-  GRBDecode ; Decode GRB Data To VRAM
+  RLEDecode ; Decode RLE DIFF Data To WRAM
 
   ; Wait For Timer
   mov r11,IO ; GBA I/O Base Offset
   orr r11,TM1CNT ; Timer Control Register
-  imm16 r10,$410 ; R10 = Time
+  imm16 r10,$3C4 ; R10 = Time
   WaitTimer:
     ldrh r12,[r11] ; Current Timer Position
     cmp r12,r10 ; Compare Time
@@ -451,15 +456,15 @@ LoopFrames:
   mov r12,TM_DISABLE
   str r12,[r11] ; Reset Timer Control
 
-  ldr r0,[LZOffset] ; Load Last LZ IPS Frame End Offset
+  GRBDecode ; Decode GRB Data To VRAM
+
+  ldr r0,[LZOffset] ; Load Last LZ Frame End Offset
   imm32 r1,SOUND ; Load Video End Offset
   cmp r0,r1 ; Check Video End Offset
   bne LoopFrames ; Decode Next Frame
   StopSoundA ; Stop Sound Channel A
   b start ; Restart Video
 
-IPSOffset: ; IPS WRAM End Of File Offset
-dw 0
 LZOffset: ; LZ ROM End Of File Offset
 dw 0
 

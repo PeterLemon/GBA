d1ccd2b7383a96511f73c044437560b6f005cc37
peterlemon <krom@gbadev.org>
2020-05-18 07:00:31 +0100

    BIOS Function Arithmetic ARCTAN2 Demo

--- BIOS/Arithmetic/ARCTAN/BIOSARCTAN.asm
+++ BIOS/Arithmetic/ARCTAN/BIOSARCTAN.asm
@@ -83,6 +83,24 @@ start:
   imm16 r1,0000001000000000b ; BG Tile Offset = 0, Tiles 4BPP, BG Map Offset = 4096, Map Size = 32x32 Tiles
   str r1,[r0,BG0CNT]
 
+  imm32 r0,$FEDCBA98
+  imm32 r1,$12345678
+
+  mov r10,IO ; GBA I/O Base Offset
+  orr r11,r10,TM0CNT             ; Timer 0 Control Register
+  mov r12,TM_ENABLE or TM_FREQ_1 ; Timer 0 Enable, Frequency/1
+  str r12,[r11]                  ; Start Timer 0
+
+  swi $0A0000 ; BIOS Function
+
+  ldr r10,[r11] ; Load  Timer 0 Value
+  imm32 r9,TIMERARCTAN2
+  str r10,[r9]  ; Store Timer 0 Value
+  imm32 r9,VALUEARCTAN2
+  str r0,[r9]  ; Store Timer 0 Value
+  mov r12,TM_DISABLE
+  str r12,[r11] ; Reset Timer 0 Control
+
   mov r0,VPAL        ; Load Color Mem Address
   imm32 r1,$7FFF7C00 ; Load  BG Font White Palette & Blue BG Color Zero
   str r1,[r0],32     ; Store BG Font Palette To Color Mem, Increment Color Mem Address To Next 4BPP Palette
@@ -160,79 +178,69 @@ start:
 
 
   PrintString LineBreakTEXT, 4544, 30, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString ArcTan2TEXT, 4608, 28, 5 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-
-;  imm32 r0,$FEDCBA98
-;  imm32 r1,$12345678
-
-;  PrintString InputTEXT, 4676, 5, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString R0TEXT, 4688, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  imm32 r9,VALUE
-;  str r0,[r9]
-;  PrintValue VALUE, 4698, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
-
-;  PrintString InputTEXT, 4740, 5, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString R1TEXT, 4752, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  imm32 r9,VALUE
-;  str r1,[r9]
-;  PrintValue VALUE, 4762, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
-
-;  PrintString TestTEXT, 4786, 4, 3 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString LineBreakTEXT, 4850, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-
-;  mov r10,IO ; GBA I/O Base Offset
-;  orr r11,r10,TM0CNT             ; Timer 0 Control Register
-;  mov r12,TM_ENABLE or TM_FREQ_1 ; Timer 0 Enable, Frequency/1
-;  str r12,[r11]                  ; Start Timer 0
-
-;  swi $0A0000 ; BIOS Function
-
-;  ldr r10,[r11] ; Load  Timer 0 Value
-;  imm32 r9,TIMER
-;  str r10,[r9]  ; Store Timer 0 Value
-;  mov r12,TM_DISABLE
-;  str r12,[r11] ; Reset Timer 0 Control
-
-;  PrintString OutputTEXT, 4866, 6, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString R0TEXT, 4880, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  imm32 r9,VALUE
-;  str r0,[r9]
-;  PrintValue VALUE, 4890, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
-
-;  imm32 r9,$00003FFF ; Load Test Check
-;  cmp r0,r9          ; Compare Result
-;  bne TestFAILC      ; IF(Check != Result) FAIL, ELSE PASS
-;  PrintString PassTEXT, 4914, 4, 2 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  b TestENDC
-;  TestFAILC:
-;    PrintString FailTEXT, 4914, 4, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  TestENDC:
-
-;  PrintString OutputTEXT, 4930, 6, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintString TimerTEXT, 4944, 8, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  PrintValue TIMER, 4962, 2, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
-
-;  imm32 r9,TIMER ; Load Result
-;  ldr r4,[r9]
-;  lsl r4,16
-;  lsr r4,16
-;  imm16 r9,$006A ; Load Test Check
-;  cmp r4,r9      ; Compare Result
-;  bne TestFAILD  ; IF(Check != Result) FAIL, ELSE PASS
-;  PrintString PassTEXT, 4978, 4, 2 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  b TestENDD
-;  TestFAILD:
-;    PrintString FailTEXT, 4978, 4, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
-;  TestENDD:
+  PrintString ArcTan2TEXT, 4608, 28, 5 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
 
 
-;  PrintString LineBreakTEXT, 4992, 30, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  imm32 r0,$FEDCBA98
+  imm32 r1,$12345678
+
+  PrintString InputTEXT, 4676, 5, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintString R0TEXT, 4688, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  imm32 r9,VALUE
+  str r0,[r9]
+  PrintValue VALUE, 4698, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
+
+  PrintString InputTEXT, 4740, 5, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintString R1TEXT, 4752, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  imm32 r9,VALUE
+  str r1,[r9]
+  PrintValue VALUE, 4762, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
+
+  PrintString TestTEXT, 4786, 4, 3 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintString LineBreakTEXT, 4850, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+
+  PrintString OutputTEXT, 4866, 6, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintString R0TEXT, 4880, 4, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintValue VALUEARCTAN2, 4890, 4, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
+
+  imm32 r9,VALUEARCTAN2 ; Load Result
+  ldr r0,[r9]
+  imm32 r9,$00003FFF ; Load Test Check
+  cmp r0,r9          ; Compare Result
+  bne TestFAILC      ; IF(Check != Result) FAIL, ELSE PASS
+  PrintString PassTEXT, 4914, 4, 2 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  b TestENDC
+  TestFAILC:
+    PrintString FailTEXT, 4914, 4, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  TestENDC:
+
+  PrintString OutputTEXT, 4930, 6, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintString TimerTEXT, 4944, 8, 0 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  PrintValue TIMERARCTAN2, 4962, 2, 2 ; Print Value: Source Address, VRAM Destination, Value Length, Palette Number
+
+  imm32 r9,TIMERARCTAN2 ; Load Result
+  ldr r4,[r9]
+  lsl r4,16
+  lsr r4,16
+  imm16 r9,$00C8 ; Load Test Check
+  cmp r4,r9      ; Compare Result
+  bne TestFAILD  ; IF(Check != Result) FAIL, ELSE PASS
+  PrintString PassTEXT, 4978, 4, 2 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  b TestENDD
+  TestFAILD:
+    PrintString FailTEXT, 4978, 4, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
+  TestENDD:
+
+
+  PrintString LineBreakTEXT, 4992, 30, 1 ; Print String: Source Address, VRAM Destination, String Length, Palette Number
 
 Loop:
   b Loop
 
 VALUE: dw 0
 TIMER: dw 0
+VALUEARCTAN2: dw 0
+TIMERARCTAN2: dw 0
 
 endcopy: ; End Of Program Copy Code
 
@@ -242,7 +250,7 @@ FONTIMG: file 'Font8x8.img'      ; Include BG 4BPP 8x8 Tile Font Character Data 
 TitleTEXT:     db "GBA BIOS Arithmetic Functions"  ; Include BG Map Text Data (29 Bytes)
 LineBreakTEXT: db "------------------------------" ; Include BG Map Text Data (30 Bytes)
 ArcTanTEXT:    db "ARCTAN $09 (Arc Tangent):"      ; Include BG Map Text Data (25 Bytes)
-;ArcTan2TEXT:   db "ARCTAN2 $0A (Arc Tangent 2):"   ; Include BG Map Text Data (28 Bytes)
+ArcTan2TEXT:   db "ARCTAN2 $0A (Arc Tangent 2):"   ; Include BG Map Text Data (28 Bytes)
 
 InputTEXT:  db "INPUT"    ; Include BG Map Text Data (5 Bytes)
 OutputTEXT: db "OUTPUT"   ; Include BG Map Text Data (6 Bytes)

a0c97f6ce523a4ca9a9d21b707c01d311924f9fb
peterlemon <krom@gbadev.org>
2020-05-23 01:50:28 +0100

    Cleanup Demos

--- Sound/BRR/SingleShot/MONO/SoundBRRMonoSingleShot.asm
+++ Sound/BRR/SingleShot/MONO/SoundBRRMonoSingleShot.asm
@@ -10,16 +10,16 @@ b copycode
 times $80000C0-($-0) db 0
 
 copycode:
-  adr r1,startcode
-  mov r2,IWRAM
-  imm32 r3,endcopy
+  adr r0,startcode
+  mov r1,IWRAM
+  imm32 r2,endcopy
   clp:
-    ldr r0,[r1],4
-    str r0,[r2],4
-    cmp r2,r3
+    ldr r3,[r0],4
+    str r3,[r1],4
+    cmp r1,r2
     bmi clp
-  imm32 r2,start
-  bx r2
+  imm32 r0,start
+  bx r0
 startcode:
   org IWRAM
 
@@ -171,7 +171,7 @@ start:
 endcopy: ; End Of Program Copy Code
 
 ; Static Data (ROM)
-org $80000C0 + (endcopy - IWRAM) + (startcode - copycode)
+org startcode + (endcopy - start)
 SOUND: ; 16-Bit 32000Hz Signed Mono Compressed BRR Sound Sample
 file 'audio.brr'
 SOUNDEND:
